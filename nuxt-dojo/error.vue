1dc3b220268b904e7a806c5d612fa11320e04e14
Vince <vincedescamps@gmail.com>
2024-11-08 14:03:06 +0100

    first commit

---
+++ nuxt-dojo/error.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="text-center mt-7 max-w-sm mx-auto card">
+		<p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
+		<p class="mt-7 text-6xl font-bold">Ooops.</p>
+		<p class="mt-7 text-6xl font-bold">Something went wrong!</p>
+		<p class="mt-7 text-6xl font-bold">:(</p>
+		<p class="mt-7">{{ error.message }}</p>
+		<button class="btn mt-7" @click="handleClearError">Go Home...</button>
+	</div>
+</template>
+
+<script setup>
+	defineProps(["error"])
+
+	const handleClearError = () => clearError({ redirect: "/" })
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxt-dojo/pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quos laudantium est
+      provident ratione facere quas totam cumque ducimus! Culpa voluptates quaerat ratione
+      officiis nesciunt mollitia odio voluptatum quibusdam officia.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quos laudantium est
+      provident ratione facere quas totam cumque ducimus! Culpa voluptates quaerat ratione
+      officiis nesciunt mollitia odio voluptatum quibusdam officia.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
